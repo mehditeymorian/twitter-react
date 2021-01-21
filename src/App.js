d3649f2698d373a077203d0b02368d75f406712f
Mehdi <mehditeymorian322@gmail.com>
2021-01-21 16:18:30 +0330

    change route for auth from home to auth

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import {hot} from "react-hot-loader";
 import React from "react";
 import './App.css';
-import HomeAuth from "./components/HomeAuth";
+import Auth from "./components/Auth";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 
 function App() {
     return (
         <Router>
             <div className="App">
-                <Route path={"/home"} component={HomeAuth} />
+                <Route path={"/auth"} component={Auth} />
             </div>
         </Router>
     );

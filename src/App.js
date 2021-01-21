e6339eb6158106d7f0d552829f4babb306de5e4d
Mehdi <mehditeymorian322@gmail.com>
2021-01-21 12:44:50 +0330

    add test route for HomeAuth

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import {hot} from "react-hot-loader";
 import React from "react";
 import './App.css';
 import HomeAuth from "./components/HomeAuth";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <HomeAuth />
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Route path={"/home"} component={HomeAuth} />
+            </div>
+        </Router>
+    );
 }
 
 export default hot(module)(App);

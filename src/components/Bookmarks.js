8437a85d1c719ddce1a725ac56c7638c69ec21bb
Mehdi <mehditeymorian322@gmail.com>
2021-01-21 21:59:48 +0330

    remove wrong import

--- src/components/Bookmarks.js
+++ src/components/Bookmarks.js
@@ -1,5 +1,4 @@
 import React from "react";
-import './App.css';
 
 export default function Bookmarks() {
     return (
--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,5 +1,4 @@
 import React from "react";
-import './App.css';
 
 export default function Messages() {
     return (

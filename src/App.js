4d555619e934ff9b5867d543c78b215d472c5546
Mehdi <mehditeymorian322@gmail.com>
2021-01-21 13:46:52 +0330

    remove unused imports

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {hot} from "react-hot-loader";
 import React from "react";
 import './App.css';
 import HomeAuth from "./components/HomeAuth";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 function App() {
     return (

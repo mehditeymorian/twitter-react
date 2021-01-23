ab5801cad1848a48d38717dd247e7fe64a806894
Mehdi <mehditeymorian322@gmail.com>
2021-01-23 09:39:59 +0330

    remove redundant container nesting

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -13,58 +13,53 @@ export default function Profile() {
     let {url} = useRouteMatch();
 
     return (
-        <fragment className={classes.root}>
-            <Paper>
-                <Grid container spacing={0}>
-                    <Grid item xs={12}>
-                        <Avatar src={"https://uifaces.co/our-content/donated/gPZwCbdS.jpg"} className={classes.profileImage}/>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <img src={"https://source.unsplash.com/random"} className={classes.image} alt={"Header Image"}/>
-                    </Grid>
-                    <Grid item xs={10}/>
-                    <Grid item xs={2}><Button className={classes.editButton}>Edit profile</Button></Grid>
-                    <Grid item xs={12}><Typography className={classes.bio}>Mehdi</Typography></Grid>
-                    <Grid item xs={12}><Typography className={classes.bio}>@meyti_T</Typography></Grid>
-                    <Grid item xs={12}>
-                        <Typography className={classes.bio}>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Fragment>
-                            <Tabs value={value} onChange={handleChange}
-                                  indicatorColor="primary" textColor="primary" centered>
-                                <Tab label="Tweets" component={Link} to={`${url}`}/>
-                                <Tab label="Tweets & replies" component={Link} to={`${url}/with_replies`}/>
-                                <Tab label="Media" component={Link} to={`${url}/media`}/>
-                                <Tab label="Likes" component={Link} to={`${url}/likes`}/>
-                            </Tabs>
-                            <Divider />
-                            <Switch>
-                                <Route exact path={`${url}`}>
-                                    <Tweet />
-                                    <Tweet />
-                                    <Tweet />
-                                    <Tweet />
-                                    <Tweet />
-                                </Route>
-                                <Route path={`${url}/with_replies`}><h1>With Replies</h1></Route>
-                                <Route path={`${url}/media`}><h1>Media</h1></Route>
-                                <Route path={`${url}/likes`}><h1>Like</h1></Route>
-                            </Switch>
-
-                        </Fragment>
-                    </Grid>
+        <Paper className={classes.root}>
+            <Grid container spacing={0}>
+                <Grid item xs={12}>
+                    <Avatar src={"https://uifaces.co/our-content/donated/gPZwCbdS.jpg"}
+                            className={classes.profileImage}/>
                 </Grid>
-            </Paper>
-            <Paper>
+                <Grid item xs={12}>
+                    <img src={"https://source.unsplash.com/random"} className={classes.image} alt={"Header Image"}/>
+                </Grid>
+                <Grid item xs={10}/>
+                <Grid item xs={2}><Button className={classes.editButton}>Edit profile</Button></Grid>
+                <Grid item xs={12}><Typography className={classes.bio}>Mehdi</Typography></Grid>
+                <Grid item xs={12}><Typography className={classes.bio}>@meyti_T</Typography></Grid>
+                <Grid item xs={12}>
+                    <Typography className={classes.bio}>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <Fragment>
+                        <Tabs value={value} onChange={handleChange}
+                              indicatorColor="primary" textColor="primary" centered>
+                            <Tab label="Tweets" component={Link} to={`${url}`}/>
+                            <Tab label="Tweets & replies" component={Link} to={`${url}/with_replies`}/>
+                            <Tab label="Media" component={Link} to={`${url}/media`}/>
+                            <Tab label="Likes" component={Link} to={`${url}/likes`}/>
+                        </Tabs>
+                        <Divider/>
+                        <Switch>
+                            <Route exact path={`${url}`}>
+                                <Tweet/>
+                                <Tweet/>
+                                <Tweet/>
+                                <Tweet/>
+                                <Tweet/>
+                            </Route>
+                            <Route path={`${url}/with_replies`}><h1>With Replies</h1></Route>
+                            <Route path={`${url}/media`}><h1>Media</h1></Route>
+                            <Route path={`${url}/likes`}><h1>Like</h1></Route>
+                        </Switch>
 
-            </Paper>
-        </fragment>
-
+                    </Fragment>
+                </Grid>
+            </Grid>
+        </Paper>
     );
 }
 

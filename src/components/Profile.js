0450ee789fe09e91e11ed42b9b1b744e60fc2c04
Mehdi <mehditeymorian322@gmail.com>
2021-01-23 21:44:28 +0330

    fix profile image position / overall design improvement

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,13 @@
 import React, {Fragment} from "react";
 import {useState} from "react";
-import {Grid, Avatar, Button, Paper, Typography, Tabs, Tab} from "@material-ui/core";
+import {Grid, Avatar, Button, Paper, Typography, Tabs, Tab, Divider} from "@material-ui/core";
 import {ProfileStyle} from "./ProfileStyle";
 import {Switch, Route, Link, useRouteMatch} from "react-router-dom";
-import Divider from "@material-ui/core/Divider";
+import {
+    LocationOn as LocationIcon,
+    Cake as BirthdayIcon,
+    CalendarToday as JoinDateIcon
+} from "@material-ui/icons";
 import Tweet from "./Tweet";
 
 export default function Profile() {
@@ -11,27 +15,34 @@ export default function Profile() {
     const [value, setValue] = useState(0);
     const handleChange = (event, newValue) => setValue(newValue);
     let {url} = useRouteMatch();
-
     return (
         <Paper className={classes.root}>
+
+
             <Grid container spacing={0}>
-                <Grid item xs={12}>
-                    <Avatar src={"https://uifaces.co/our-content/donated/gPZwCbdS.jpg"}
-                            className={classes.profileImage}/>
-                </Grid>
-                <Grid item xs={12}>
-                    <img src={"https://source.unsplash.com/random"} className={classes.image} alt={"Header Image"}/>
-                </Grid>
-                <Grid item xs={10}/>
-                <Grid item xs={2}><Button className={classes.editButton}>Edit profile</Button></Grid>
-                <Grid item xs={12}><Typography className={classes.bio}>Mehdi</Typography></Grid>
-                <Grid item xs={12}><Typography className={classes.bio}>@meyti_T</Typography></Grid>
+                <Grid item xs={12}><img src={"https://source.unsplash.com/random"} className={classes.image}
+                                        alt={"Header Image"}/></Grid>
+                <Grid item xs={9}><Avatar src={"https://uifaces.co/our-content/donated/gPZwCbdS.jpg"}
+                                          className={classes.profileImage}/></Grid>
+                <Grid item xs={3}><Button className={classes.editButton} variant={"outlined"}>Edit
+                    profile</Button></Grid>
+                <Grid item xs={12}><Typography className={classes.userName}>Mehdi</Typography></Grid>
+                <Grid item xs={12}><Typography className={classes.bio} variant={"caption"}>@meyti_T</Typography></Grid>
                 <Grid item xs={12}>
                     <Typography className={classes.bio}>
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                         when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                     </Typography>
+                </Grid>
+                <Grid container xs={12} className={classes.bioInfoLayout}>
+                    <Typography display={"inline"} className={classes.bioInfo}><LocationIcon className={classes.bioInfoIcon}/>Mars</Typography>
+                    <Typography display={"inline"} className={classes.bioInfo}><BirthdayIcon className={classes.bioInfoIcon}/>Born December 13, 1999</Typography>
+                    <Typography display={"inline"} className={classes.bioInfo}><JoinDateIcon className={classes.bioInfoIcon}/>Joined August 2016</Typography>
+                </Grid>
+                <Grid container xs={12} className={classes.bioInfoLayout}>
+                    <Typography component={Link} to={"/followings"} className={classes.bioInfo}>192 Followings</Typography>
+                    <Typography component={Link} to={"/followers"} className={classes.bioInfo}>173 Followers</Typography>
                 </Grid>
                 <Grid item xs={12}>
                     <Fragment>
--- src/components/ProfileStyle.js
+++ src/components/ProfileStyle.js
@@ -4,24 +4,49 @@ import {makeStyles} from "@material-ui/core";
 export const ProfileStyle =  makeStyles((theme) => ({
     root: {
         width: "100%",
+        position: "relative",
+        borderRadius: "8px"
     },
     image: {
         width: "100%",
         height: "250px",
-
+        borderTopLeftRadius: "8px",
+        borderTopRightRadius: "8px"
     },
     profileImage: {
         width: theme.spacing(12),
         height: theme.spacing(12),
+        zIndex: 20,
         position: "absolute",
-        top: "315px",
-        left: "5px"
+        top: "200px",
+        left: "5%"
     },
     editButton: {
-        color: "white"
+        color: "black",
+        marginTop: theme.spacing(2),
+        borderRadius: "16px"
     },
-
+    userName: {
+        padding: theme.spacing(0,2),
+        fontWeight: "bold"
+    },
     bio:{
         padding: theme.spacing(0,2)
+    },
+    bioInfoLayout: {
+        paddingLeft: theme.spacing(2),
+        paddingRight: theme.spacing(2),
+        paddingTop: theme.spacing(1),
+    },
+    bioInfo: {
+        marginRight: theme.spacing(2),
+        alignItems: "center",
+        textDecoration: "none"
+    },
+    bioInfoIcon: {
+        position: "relative",
+        marginRight: theme.spacing(1),
+        width: theme.typography.body1.fontSize,
+        height: theme.typography.body1.fontSize
     }
 }));

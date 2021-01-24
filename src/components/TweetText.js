cc1e2d177e121d55747c99f699c1c63bfd0c873b
Mehdi <mehditeymorian322@gmail.com>
2021-01-24 15:46:48 +0330

    create tweet text for highlighting hashtags and urls

--- src/components/TweetText.js
+++ src/components/TweetText.js
@@ -1,12 +1,25 @@
 import React from "react";
+import {makeStyles} from "@material-ui/core";
 
 
 const URL_REGEX = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi;
 const HASHTAG_REGEX = /#[\w_]+/gi;
 
-export default function TweetText({value,outputStyle}) {
+const Classes = makeStyles(() => ({
+    root: {
+        width: "100%",
+        position: "absolute",
+        top: "5px",
+        fontSize: "medium",
+        lineHeight: "20px",
+
+    }
+}));
+
+export default function TweetText({value}) {
+    const style = Classes();
     let result = value.replaceAll(HASHTAG_REGEX, oldValue => `<span><a href={"/hashtag"}>${oldValue}</a></span>`);
     result = result.replaceAll(URL_REGEX, oldValue => `<span><a href={"/url"}>${oldValue}</a></span>`);
-    return (<div className={outputStyle} dangerouslySetInnerHTML={{__html: result}}/>);
+    return (<div className={style.root} dangerouslySetInnerHTML={{__html: result}}/>);
 
 }

64e9cd6aac205a9053a449ac9f764a78036af137
Mehdi <mehditeymorian322@gmail.com>
2021-01-24 15:47:27 +0330

    create special textField for highlighting hashtags and urls on user input

---
+++ src/components/SpecialTextField.js
@@ -0,0 +1,49 @@
+import TextField from "@material-ui/core/TextField";
+import React, {useState} from "react";
+import {makeStyles} from "@material-ui/core";
+import TweetText from "./TweetText";
+
+const classes = makeStyles(() => ({
+    textInput: {
+        fontSize: "medium",
+        lineHeight: "20px",
+        color: "transparent",
+        caretColor: "black",
+        zIndex: "1",
+        "&::placeholder": {
+            color: "#434343"
+        }
+    },
+    textInputLayout: {
+        position: "relative",
+        zIndex: "0"
+    },
+
+}));
+
+
+
+
+
+export default function SpecialTextField() {
+    const style = classes();
+    const [input, setInput] = useState("");
+
+
+    const onInputChange = input => setInput(input.target.value);
+
+    return (
+        <div className={style.textInputLayout}>
+            <TextField
+                placeholder={"What's happening?"}
+                onInput={onInputChange}
+                fullWidth
+                multiline
+                required
+                rows={5}
+                inputProps={{maxLength: 250, className: style.textInput}}
+            />
+            <TweetText value={input}/>
+        </div>
+    );
+}

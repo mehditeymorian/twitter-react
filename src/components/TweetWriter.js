783a634ec3a6d80fdd4cb96c1a0084e36adcfd02
Mehdi <mehditeymorian322@gmail.com>
2021-01-24 15:48:17 +0330

    replace old jsx with SpecialTextField component

--- src/components/TweetWriter.js
+++ src/components/TweetWriter.js
@@ -15,9 +15,11 @@ import {TweetWriterStyle} from "./TweetWriterStyle";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Divider from "@material-ui/core/Divider";
+import SpecialTextField from "./SpecialTextField";
 
 export default function TweetWriter() {
     const style = TweetWriterStyle();
+
 
     return (
         <>
@@ -25,31 +27,21 @@ export default function TweetWriter() {
                 <Grid item xs={1}><Avatar src={"https://uifaces.co/our-content/donated/gPZwCbdS.jpg"}/></Grid>
                 <Grid item xs={11}>
                     <Grid container>
-                        <Grid item xs={12}>
-                            <TextField
-                                placeholder={"What's happening?"}
-                                fullWidth
-                                multiline
-                                required
-                                rows={5}
-                                inputProps={{maxLength: 250}}
-                            /></Grid>
-                        <Grid item xs={12}>
-                            <Grid container alignItems={"center"} className={style.actionsLayout}>
-                                <Grid item xs={10}>
-                                    <Grid container>
-                                        <Grid item><IconButton><MediaIcon/></IconButton></Grid>
-                                        <Grid item><IconButton><GifIcon/></IconButton></Grid>
-                                        <Grid item><IconButton><PollIcon/></IconButton></Grid>
-                                        <Grid item><IconButton><EmojiIcon/></IconButton></Grid>
-                                        <Grid item><IconButton><ScheduleIcon/></IconButton></Grid>
-                                    </Grid>
-                                </Grid>
-                                <Grid item xs={2}>
-                                    <Button variant={"contained"}
-                                            color={"secondary"}
-                                    >Tweet</Button>
-                                </Grid>
+                        <Grid item xs={12}><SpecialTextField/></Grid>
+                        <Grid container alignItems={"center"} className={style.actionsLayout}>
+                            <Grid container xs={10}>
+                                <Grid item><IconButton><MediaIcon/></IconButton></Grid>
+                                <Grid item><IconButton><GifIcon/></IconButton></Grid>
+                                <Grid item><IconButton><PollIcon/></IconButton></Grid>
+                                <Grid item><IconButton><EmojiIcon/></IconButton></Grid>
+                                <Grid item><IconButton><ScheduleIcon/></IconButton></Grid>
+                            </Grid>
+                            <Grid item xs={2}>
+                                <Button variant={"contained"}
+                                        disableElevation
+                                        color={"secondary"}
+                                        className={style.sendButton}
+                                >Tweet</Button>
                             </Grid>
                         </Grid>
                     </Grid>

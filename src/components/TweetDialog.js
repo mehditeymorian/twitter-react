30ce21df8fd5ada117712f7ada9e98862e2fe85c
Mehdi <mehditeymorian322@gmail.com>
2021-01-25 12:31:44 +0330

    create tweet dialog component

---
+++ src/components/TweetDialog.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {useMediaQuery, useTheme} from "@material-ui/core";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import TweetWriter from "./TweetWriter";
+
+
+export default function TweetDialog({open,setOpen}) {
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
+    const handleClose = () => setOpen(false);
+
+
+    return (
+        <Dialog
+            fullScreen={fullScreen}
+            open={open}
+            fullWidth
+            maxWidth={"sm"}
+            onClose={handleClose}
+            aria-labelledby="responsive-dialog-title">
+            <DialogTitle id="responsive-dialog-title">{"Tweet"}</DialogTitle>
+            <DialogContent>
+                <TweetWriter />
+            </DialogContent>
+        </Dialog>
+    );
+
+};

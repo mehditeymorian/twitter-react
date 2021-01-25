53f08fca5bec89f8660861b939475e957fcedda8
Mehdi <mehditeymorian322@gmail.com>
2021-01-25 21:28:19 +0330

    fix route overlap

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import {hot} from "react-hot-loader";
 import React from "react";
 import './App.css';
 import Auth from "./components/Auth";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Main from "./components/Main";
 
 
 function App() {
     return (
-        /*todo: auth and Main have route overlap*/
         <Router>
             <div className="App">
-                <Route path={"/auth"} render={props=>( <Auth {...props} routes={[]} />)} />
-                <Route path={"/"} render={props=>( <Main {...props} routes={[]} />)} />
+                <Switch>
+                <Route path={"/auth/"} render={props=>( <Auth {...props} routes={[]} />)} />
+                <Route path={`/`} render={props=>( <Main {...props} routes={[]} />)} />
+                </Switch>
             </div>
         </Router>
     );

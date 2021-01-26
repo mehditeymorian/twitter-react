4894a23552a326f45fc8f43fc190c504abdb3655
Mehdi <mehditeymorian322@gmail.com>
2021-01-26 22:44:08 +0330

    connect TweetWriter to redux

--- src/components/TweetWriter.js
+++ src/components/TweetWriter.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useState} from "react";
 import Grid from "@material-ui/core/Grid";
 import Avatar from "@material-ui/core/Avatar";
 import Typography from "@material-ui/core/Typography";
@@ -16,17 +16,30 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Divider from "@material-ui/core/Divider";
 import SpecialTextField from "./SpecialTextField";
+import {createTweet} from "../redux/actions";
+import {connect} from "react-redux";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import {isStateLoading} from "../redux/stateUtils";
 
-export default function TweetWriter() {
+function TweetWriter({userState, createState, createTweet}) {
     const style = TweetWriterStyle();
+    const [tweetText, setTweetText] = useState("");
 
+    const onSend = ev => {
+        const tweet = {
+            text: tweetText,
+            media: null,
+            parent: null
+        };
+        createTweet(tweet);
+    };
 
     return (
         <>
             <Grid container className={style.root}>
-                <Grid item xs={2} md={1}><Avatar src={"https://uifaces.co/our-content/donated/gPZwCbdS.jpg"}/></Grid>
+                <Grid item xs={2} md={1}><Avatar src={userState.profile_picture} alt={userState.username}/></Grid>
                 <Grid container xs={10} md={11}>
-                    <Grid item xs={12}><SpecialTextField/></Grid>
+                    <Grid item xs={12}><SpecialTextField text={tweetText} setText={setTweetText}/></Grid>
                     <Grid container alignItems={"center"} justify={"space-between"} className={style.actionsLayout}>
                         <Grid container  xs>
                             <Grid item><IconButton><MediaIcon/></IconButton></Grid>
@@ -36,12 +49,24 @@ export default function TweetWriter() {
                             <Grid item><IconButton><ScheduleIcon/></IconButton></Grid>
                         </Grid>
                         <Grid item xs={3} sm={3} md={2} lg={2}>
-                            <Button variant={"contained"} disableElevation color={"secondary"} className={style.sendButton}>Tweet</Button>
+                            <Button variant={"contained"} onClick={onSend} disableElevation color={"secondary"} className={style.sendButton}>Tweet</Button>
                         </Grid>
                     </Grid>
                 </Grid>
             </Grid>
+            {isStateLoading(createState)? <LinearProgress /> : null}
             <Divider/>
         </>
     );
 };
+
+const mapStateToProp = state => ({
+    createState: state.createTweet,
+    userState: state.user
+});
+
+const mapActionsToProp = dispatch => ({
+    createTweet: tweet => dispatch(createTweet(tweet)),
+});
+
+export default connect(mapStateToProp, mapActionsToProp)(TweetWriter);

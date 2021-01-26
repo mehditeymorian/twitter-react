4ff46b2ed10b95db4952f98e93d3059825301d05
Mehdi <mehditeymorian322@gmail.com>
2021-01-26 21:10:00 +0330

    persist user

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {PersistGate} from 'redux-persist/lib/integration/react';
 import {getStore} from "./redux/store";
 import {Provider} from "react-redux";
+import {persistStore} from "redux-persist";
 
 
 const store = getStore();
+const persistor = persistStore(store);
 
 ReactDOM.render(
     <Provider store={store}>
         <React.StrictMode>
-            <App/>
+            <PersistGate
+                loading={<div>Loading...</div>}
+                persistor={persistor}>
+                <App/>
+            </PersistGate>
         </React.StrictMode>
     </Provider>,
     document.getElementById('root')
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,7 @@
 import {combineReducers, createStore, applyMiddleware} from 'redux';
+import {persistReducer} from "redux-persist";
+import storage from 'redux-persist/lib/storage'
+import {autoMergeLevel2} from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import {authReducer as user} from "./reducers";
 import thunk from "redux-thunk";
@@ -8,5 +11,14 @@ const reducers = combineReducers({
     user,
 });
 
+const persistConfig = {
+    key: 'twitter',
+    blacklist: [],
+    storage,
+    stateReconciler : autoMergeLevel2
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 const devTool = composeWithDevTools(applyMiddleware(thunk));
-export const getStore = () => createStore(reducers, devTool);
+export const getStore = () => createStore(persistedReducer, devTool);

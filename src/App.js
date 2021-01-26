7c2b28ada73d3bcc89b4f9468069d7a60894e42f
Mehdi <mehditeymorian322@gmail.com>
2021-01-26 11:48:53 +0330

    add redirection for not logged user and logged user

--- src/App.js
+++ src/App.js
@@ -2,22 +2,32 @@ import {hot} from "react-hot-loader";
 import React from "react";
 import './App.css';
 import Auth from "./components/Auth";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
 import Main from "./components/Main";
+import {connect} from "react-redux";
 
 
-function App() {
+function App({userState}) {
+
+    const logged = userState !== null;
+
     return (
         <Router>
             <div className="App">
                 <Switch>
-                <Route path={"/auth/"} render={props=>( <Auth {...props} routes={[]} />)} />
-                <Route path={`/`} render={props=>( <Main {...props} routes={[]} />)} />
+                    <Route exact path={"/auth"}><Redirect to={"/auth/sign-in"}/></Route>
+                    <Route path={"/auth/"}>{logged ? <Redirect to={"/"}/>: <Auth/>}</Route>
+                    <Route path={`/`}>{logged ? <Main/> : <Redirect to={"/auth/sign-in"}/>}</Route>
                 </Switch>
             </div>
         </Router>
     );
 }
 
+const mapStateToProp = state => ({
+    userState: state.user
+});
 
-export default hot(module)(App);
+const mapActionsToProp = dispatch => ({});
+
+export default hot(module)(connect(mapStateToProp, mapActionsToProp)(App));

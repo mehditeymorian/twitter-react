c7769e94a82e8e435f494bbb2d19ca74f8740796
Mehdi <mehditeymorian322@gmail.com>
2021-01-26 11:48:16 +0330

    connect auth to redux store

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,31 +2,39 @@ import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import {Switch, Route, useRouteMatch} from 'react-router-dom'
+import {Switch, Route, useRouteMatch, Redirect} from 'react-router-dom'
 import SignIn from './SignIn'
 import SignUp from "./SignUp";
 import {AuthStyle} from "./AuthStyle";
+import {connect} from "react-redux";
 
 
 
-export default function Auth() {
+function Auth({userState}) {
     const classes = AuthStyle();
     let {url} = useRouteMatch();
+    let logged = userState !== null;
 
     return (
         <Grid container component="main" className={classes.root}>
             <CssBaseline/>
             <Grid item xs={false} sm={4} md={7} className={classes.image}/>
             <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-                {/*{section === "sign-up" ? <SignUp /> : <SignIn />}*/}
-
                 <Switch>
-                    <Route exact path={`${url}`} component={SignIn} />
-                    <Route path={`${url}/sign-in`} component={SignIn}/>
-                    <Route path={`${url}/sign-up`} component={SignUp}/>
+                    <Route path={`${url}/sign-in`}>{logged ? <Redirect to={"/home"} /> : <SignIn />}</Route>
+                    <Route path={`${url}/sign-up`}>{logged ? <Redirect to={"/home"} /> : <SignUp />}</Route>
                 </Switch>
 
             </Grid>
         </Grid>
     );
 }
+
+const mapStateToProp = state => ({
+    userState: state.user
+});
+
+const mapActionsToProp = dispatch => ({
+});
+
+export default connect(mapStateToProp,mapActionsToProp)(Auth);

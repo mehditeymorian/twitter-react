21249a4bc11de25a27a16447b215a2bba309e49b
Mehdi <mehditeymorian322@gmail.com>
2021-01-26 17:17:27 +0330

    add initUser state

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,4 +1,6 @@
 import {
+    AUTH_LOADING,
+    AUTH_NULL, AUTH_SUCCESS,
     LOGOUT,
     SIGNIN_FAIL,
     SIGNIN_INIT,
@@ -8,33 +10,60 @@ import {
     SIGNUP_SUCCESS
 } from "./actions";
 
-export const authReducer = (userState = null, action) => {
+const initUser = {
+    state: AUTH_NULL
+};
+
+export const authReducer = (userState = initUser, action) => {
     const {type, payload} = action;
 
     switch (type) {
-        case SIGNUP_INIT:{
-            return userState;
+        case SIGNUP_INIT: {
+            return {
+                ...userState,
+                state: AUTH_LOADING
+            };
         }
-        case SIGNUP_FAIL:{
-            return userState;
+        case SIGNUP_FAIL: {
+            const {code} = payload;
+            return {
+                ...userState,
+                state: code
+            };
         }
-        case SIGNUP_SUCCESS:{
+        case SIGNUP_SUCCESS: {
             const {user} = payload;
-            return user;
+            return {
+                ...user,
+                state: AUTH_SUCCESS
+            };
         }
-        case SIGNIN_INIT:{
-            return userState;
+        case SIGNIN_INIT: {
+            return {
+                ...userState,
+                state: AUTH_LOADING
+            };
         }
-        case SIGNIN_SUCCESS:{
+        case SIGNIN_SUCCESS: {
             const {user} = payload;
-            return user;
+            return {
+                ...user,
+                state: AUTH_SUCCESS
+            };
         }
-        case SIGNIN_FAIL:{
+        case SIGNIN_FAIL: {
+            const {code} = payload;
+            return {
+                ...userState,
+                state: code
+            };
+        }
+        case LOGOUT: {
+            return {
+                state: AUTH_NULL
+            };
+        }
+        default:
             return userState;
-        }
-        case LOGOUT:{
-            return null;
-        }
-        default: return userState;
     }
 };

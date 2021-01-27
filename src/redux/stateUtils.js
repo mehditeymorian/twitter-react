6fe213cb381df6903db07df01cd6b53ea3456e82
Mehdi <mehditeymorian322@gmail.com>
2021-01-27 09:03:29 +0330

    helper function to create state actions

--- src/redux/stateUtils.js
+++ src/redux/stateUtils.js
@@ -1,6 +1,29 @@
-import {STATE_LOADING, STATE_SUCCESS} from "./actions";
+import {SIGNIN_INIT, STATE_LOADING, STATE_SUCCESS} from "./actions";
 
 
 export const isStatePresent = parent => parent.state === STATE_SUCCESS
 export const isStateLoading = parent => parent.state === STATE_LOADING;
 export const isStateFailed = parent => parent.state >= 1;
+
+
+export const createInit = (type) => ({
+    type: type,
+    payload: {
+        code: STATE_LOADING
+    }
+});
+
+export const createSuccess = (type, result) => ({
+    type: type,
+    payload: {
+        code: STATE_SUCCESS,
+        result: result
+    }
+});
+
+export const createFail = (type,failCode) => ({
+    type: type,
+    payload: {
+        code: failCode
+    }
+});

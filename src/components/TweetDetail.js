e85ab11fc2d25b523bdaef9238b6367ea617eac8
Mehdi <mehditeymorian322@gmail.com>
2021-01-27 19:21:26 +0330

    complete tweet detail component information loading

--- src/components/TweetDetail.js
+++ src/components/TweetDetail.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {useParams} from 'react-router-dom';
+import {getTweet} from "../redux/actions";
+import {connect} from "react-redux";
+import Tweet, {TWEET_DETAIL, TWEET_NORMAL, TWEET_REPLY} from "./Tweet";
+import {isStatePresent} from "../redux/stateUtils";
 
 
-export default function TweetDetail() {
+function TweetDetail({userState,tweetState,getTweetById}) {
+    const {id} = useParams();
+
+    useEffect(() => {
+        getTweetById(id);
+    }, [id]);
+
     return (
         <>
-            {/*<Tweet/>*/}
-            {/*<Tweet/>*/}
-            {/*<Tweet type={TWEET_DETAIL}/>*/}
-            {/*<Tweet type={TWEET_REPLY}/>*/}
-            {/*<Tweet type={TWEET_REPLY}/>*/}
+            {isStatePresent(tweetState) ? tweetState.parents.map(each=> <Tweet username={userState.username} tweet={each} /> ) : null}
+            {isStatePresent(tweetState) ? <Tweet type={TWEET_DETAIL} username={userState.username} tweet={tweetState} /> : null}
+            {isStatePresent(tweetState) ? tweetState.comments.map(each=> <Tweet type={TWEET_REPLY} username={userState.username} tweet={each} /> ) : null}
         </>
     );
 
 }
+
+const mapStateToProp = state => ({
+    userState: state.user,
+    tweetState: state.getTweet
+});
+
+const mapActionsToProp = dispatch => ({
+    getTweetById: (id) => dispatch(getTweet(id)),
+});
+
+export default connect(mapStateToProp, mapActionsToProp)(TweetDetail);
